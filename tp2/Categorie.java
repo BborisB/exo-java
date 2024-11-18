470137126119ed35d6c419aa6143497dd56c544e
BborisB <b.boris.2001@gmail.com>
2024-11-18 15:59:08 +0100

    docs(tp2): add Categorie.java

---
+++ tp2/Categorie.java
@@ -0,0 +1,18 @@
+package tp2;
+
+public class Categorie {
+	private String titre;
+
+	public Categorie(String titre)
+	{
+		this.titre = titre;
+	}
+	
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String categorie) {
+		this.titre = categorie;
+	}
+}
