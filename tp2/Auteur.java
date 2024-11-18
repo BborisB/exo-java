bc01bf89f88d5d4335477cd344bd9f6dccc76028
BborisB <b.boris.2001@gmail.com>
2024-11-18 15:57:42 +0100

    docs(tp2): add Auteur.java

---
+++ tp2/Auteur.java
@@ -0,0 +1,24 @@
+package tp2;
+
+public class Auteur {
+	private String prenom;
+	private String nom;
+	
+	public Auteur(String prenom, String nom) {
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
