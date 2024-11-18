cc21833f0c9099fb03dcc6ac3583df0e8c45faf6
BborisB <b.boris.2001@gmail.com>
2024-11-18 16:01:08 +0100

    docs(tp2): add Point.java

---
+++ tp2/Point.java
@@ -0,0 +1,36 @@
+package tp2;
+
+public class Point {
+	private float x;
+	private float y;
+	private float z;
+	
+	public Point(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public float getZ() {
+		return z;
+	}
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2) + Math.pow(p2.z - p1.z, 2));
+	}
+}
