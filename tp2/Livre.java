9c0333264a7811b46c8eb671c8c1a2f7a36c04c1
BborisB <b.boris.2001@gmail.com>
2024-12-09 15:44:52 +0100

    docs(exo4): fixed formatting in Livre.java

--- tp2/Livre.java
+++ tp2/Livre.java
@@ -5,47 +5,51 @@ public class Livre {
 	private String titre;
 	private Categorie categorie;
 	private String isbdn_13;
-	
+
 	public Livre(String titre, Auteur auteur, Categorie categorie, String isbdn_13) {
 		this.titre = titre;
 		this.auteur = auteur;
 		this.categorie = categorie;
 		this.isbdn_13 = isbdn_13;
 	}
-	
+
 	public String code() {
-		return 
-				"" + 
-				this.auteur.getPrenom().charAt(0) + 
-				this.auteur.getNom().charAt(0) + 
-				categorie.getTitre().charAt(0) +
-				(this.isbdn_13.length() > 2 ? this.isbdn_13.substring(this.isbdn_13.length() - 2) : this.isbdn_13);
+		return "" + this.auteur.getPrenom().charAt(0) + this.auteur.getNom().charAt(0) + categorie.getTitre().charAt(0)
+				+ (this.isbdn_13.length() > 2 ? this.isbdn_13.substring(this.isbdn_13.length() - 2) : this.isbdn_13);
 	}
-	
+
 	public Auteur getAuteur() {
 		return auteur;
 	}
+
 	public void setAuteur(Auteur auteur) {
 		this.auteur = auteur;
 	}
+
 	public String getTitre() {
 		return titre;
 	}
+
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
+
 	public String getCategorie() {
 		return titre;
 	}
+
 	public void setCategorie(String titre) {
 		this.titre = titre;
 	}
+
 	public void showInfos() {
 		System.out.println("Titre du livre : " + this.titre + "\nAuteur : " + this.auteur);
 	}
+
 	public String getIsbdn_13() {
 		return isbdn_13;
 	}
+
 	public void setIsbdn_13(String isbdn_13) {
 		this.isbdn_13 = isbdn_13;
 	}
